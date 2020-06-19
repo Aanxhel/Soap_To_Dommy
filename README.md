928e4b2947ca2ddfe57ff3baa48c1d4d5eaf36f4
aanxhel <ang.core92@yahoo.com>
2020-06-19 12:35:31 -0500

    intermedio de creacion de articulos en la carpeta app con articulo

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ creacermos el proyecto desde aqui
 catalogo de productos
 
 repo: https://github.com/Aanxhel/Soap_To_Dommy.git
-
+______________________________________________________
 recursos utilizados:
+
 OS: Debian 10
+
 IDE: Eclipse 2020
+
 Plugin: Angular
 
 
--- SoapDommy/src/app/articulo/articulo.component.ts
+++ SoapDommy/src/app/articulo/articulo.component.ts
@@ -1,4 +1,13 @@
 /*
 se crea articulo componentes con nomenclatura 
 [nombreProyecto].component.ts
+Articulo.component.ts
 */
+import { Component } from '@angular/core';
+
+@Component({										// decorador dentro de su mismo nucleo
+	selector: 'app-articulo',
+	templateUrl: './articulo.component.html'
+
+})
+export class ArticuloComponent 						//la clase sera [proyecto][component]
