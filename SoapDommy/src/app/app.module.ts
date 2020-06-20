3686776f17cd05f342b8d09c76c8d8342af4898d
aanxhel <ang.core92@yahoo.com>
2020-06-19 19:14:55 -0500

    se agrega componente segun angular pero no agarra

--- SoapDommy/src/app/app.module.ts
+++ SoapDommy/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticuloComponent } from './articulo/articulo.component';             //se crea componente [proyecto][component]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ArticuloComponent
   ],
   imports: [
     BrowserModule
--- SoapDommy/src/app/articulo/articulo.component.ts
+++ SoapDommy/src/app/articulo/articulo.component.ts
@@ -1,13 +1,14 @@
 /*
-se crea articulo componentes con nomenclatura 
+se crea articulo componentes con nomenclatura
 [nombreProyecto].component.ts
 Articulo.component.ts
 */
 import { Component } from '@angular/core';
 
 @Component({										// decorador dentro de su mismo nucleo
-	selector: 'app-articulo',
-	templateUrl: './articulo.component.html'
-
+selector: 'app-articulo',						// selector para decorador
+templateUrl: './articulo.component.html'
 })
-export class ArticuloComponent 						//la clase sera [proyecto][component]
+
+export class ArticuloComponent {						// la clase sera [proyecto][component]
+}
